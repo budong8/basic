0e4df1e84ff6612b7532b6a9c282c6bb25e5df8a
budong8 <1226576014@qq.com>
2020-03-27 08:29:55 +0800

    冒泡排序算法

---
+++ class_0326/class_01.py
@@ -0,0 +1,14 @@
+# 冒泡排序算法
+
+
+def bubble(mylist):
+    for i in range(len(mylist) - 1):
+        for j in range(len(mylist) - 1):
+            if mylist[j] > mylist[j + 1]:
+                mylist[j], mylist[j + 1] = mylist[j + 1], mylist[j]
+    return mylist
+
+
+if __name__ == '__main__':
+    mylist = [23, 56, 3, 5, 99, 88, 42]
+    print(bubble(mylist))
